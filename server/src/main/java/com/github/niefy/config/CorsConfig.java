e9a395c2b45383676f7e9c9672c24abf05786770
imndx <imndxx@gmail.com>
2022-09-09 22:41:01 +0800

    跨域

--- server/src/main/java/com/github/niefy/config/CorsConfig.java
+++ server/src/main/java/com/github/niefy/config/CorsConfig.java
@@ -1,15 +1,16 @@
 package com.github.niefy.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOriginPatterns("*")
             .allowCredentials(true)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .maxAge(3600);
