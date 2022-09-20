d9dd4df35a9fb7b242e2c29a74a5a65eee7e164e
imndx <imndxx@gmail.com>
2022-09-20 15:17:16 +0800

    update

--- manage/src/views/main-navbar.vue
+++ manage/src/views/main-navbar.vue
@@ -2,7 +2,7 @@
     <nav class="site-navbar" :class="'site-navbar--' + navbarLayoutType">
         <div class="site-navbar__header">
             <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })">
-                <a class="site-navbar__brand-lg" href="javascript:;">频道管理系统</a>
+                <a class="site-navbar__brand-lg" href="javascript:;">野火频道管理系统</a>
                 <a class="site-navbar__brand-mini" href="javascript:;">W</a>
             </h1>
         </div>
